e8dac9e028e626ccc631bee270e3a220bec2fd57
guillaume.chebib <guillaume.chebib@IGTP5-PC19.isim.intra>
2019-12-03 16:59:25 +0100

    change

--- testPisc/src/packageGraphique/Test.java
+++ testPisc/src/packageGraphique/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fenetre fen= new Fenetre(); //crée une instance de fenetre
+		Fenetre fen= new Fenetre(); //cree une instance de fenetre
 	}
 
 }
