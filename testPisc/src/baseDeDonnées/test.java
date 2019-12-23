77ad00e2b7d278dc1a6021a77030848eb53af5b4
Guillaume <guillaume.chebib@hotmail.fr>
2019-12-23 23:04:26 +0100

    modif

--- testPisc/src/baseDeDonnées/test.java
+++ testPisc/src/baseDeDonnées/test.java
@@ -11,14 +11,23 @@ public static void main(String[] args) {
     String url = "jdbc:postgresql://localhost:5432/piscine";
     String user = "postgres";
     String passwd = "motdepasse";
-       
+    
+    //permet la connection à la bdd   
     Connection conn = DriverManager.getConnection(url, user, passwd);
        
     //Création d'un objet Statement
     Statement state = conn.createStatement();
-    //L'objet ResultSet contient le résultat de la requête SQL
+    
+    //Ajout d'un élement dans une table
+    String sqlInsert = "INSERT INTO utilisateur VALUES (5, 'test.test@gmail.com', 'pass', false, 'IG3')";
+    //System.out.println("The SQL statement is: " + sqlInsert + "\n");  // Echo for debugging
+    /*int countInserted = */ state.executeUpdate(sqlInsert);
+    //System.out.println(countInserted + " records inserted.\n");
+    
+    
+    //On recupere le resultat d'une requete 
     ResultSet result = state.executeQuery("SELECT * FROM utilisateur");
-    //On récupère les MetaData
+    //On récupère les MetaData (les colonnes)
     ResultSetMetaData resultMeta = result.getMetaData();
        
     System.out.println("\n**********************************");
@@ -27,7 +36,8 @@ public static void main(String[] args) {
       System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
        
     System.out.println("\n**********************************");
-       
+    
+    //On affiche les elements d'une table
     while(result.next()){         
       for(int i = 1; i <= resultMeta.getColumnCount(); i++)
         System.out.print("\t" + result.getObject(i).toString() + "\t |");
@@ -36,8 +46,8 @@ public static void main(String[] args) {
 
     }
 
-    result.close();
-    state.close();
+    //result.close();
+    //state.close();
        
   } catch (Exception e) {
     e.printStackTrace();
