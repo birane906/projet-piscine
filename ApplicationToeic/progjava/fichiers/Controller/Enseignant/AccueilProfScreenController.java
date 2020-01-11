926b0cf4702be335bfa0eff2347b59f3c6ea9584
tiffany-dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2020-01-11 02:36:05 +0100

    Update AccueilProfScreenController.java

--- ApplicationToeic/progjava/fichiers/Controller/Enseignant/AccueilProfScreenController.java
+++ ApplicationToeic/progjava/fichiers/Controller/Enseignant/AccueilProfScreenController.java
@@ -1,17 +1,74 @@
-package java.fichiers.Controller.Enseignant;
+package progjava.fichiers.Controller.Enseignant;
 
+import progjava.fichiers.Config.ConnectionUtil;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class AccueilProfScreenController implements Initializable {
 	
+Stage dialogStage = new Stage();
 	
+	Connection connection = null;
+	PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
 	
-	
-	
+	public AccueilProfScreenController() {
+        connection = ConnectionUtil.connectdb();
+    }
 
+	public void DeconnexionAction(ActionEvent event) {
+		try {
+			Node node = (Node)event.getSource();
+            dialogStage = (Stage) node.getScene().getWindow();
+            dialogStage.getScene().setRoot(FXMLLoader.load(getClass().getResource("../../../../src/layout/Login/FXMLDocument.fxml")));
+		}
+		catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+	
+	public void GérerLesTOEIC(ActionEvent event) {
+		try {
+			Node node = (Node)event.getSource();
+            dialogStage = (Stage) node.getScene().getWindow();
+            dialogStage.getScene().setRoot(FXMLLoader.load(getClass().getResource("../../../../src/layout/Enseignant/Toeic/GérerLesTOEICScreen.fxml")));
+		}
+		catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+	
+	public void Resultat(ActionEvent event) {
+		try {
+			Node node = (Node)event.getSource();
+            dialogStage = (Stage) node.getScene().getWindow();
+            dialogStage.getScene().setRoot(FXMLLoader.load(getClass().getResource("../../../../src/layout/Enseignant/Statistiques/RésultatsProfScreen.fxml")));
+		}
+		catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+	
+	public void GérerLesEtudiants(ActionEvent event) {
+		try {
+			Node node = (Node)event.getSource();
+            dialogStage = (Stage) node.getScene().getWindow();
+            dialogStage.getScene().setRoot(FXMLLoader.load(getClass().getResource("../../../../src/layout/Enseignant//GestionEtudiant/GérerLesEtudiantsScreen.fxml")));
+		}
+		catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
